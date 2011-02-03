9b8bead4e323bcb524f345db3d89f530f4bee013
Michael Kessler <michi@netzpiraten.ch>
2011-02-03 15:09:30 +0100

    Change the lexer strategy to handle the incremental lexer

--- src/org/coffeebrew/lang/lexer/CoffeeScriptFlexLexer.java
+++ src/org/coffeebrew/lang/lexer/CoffeeScriptFlexLexer.java
@@ -16,8 +16,25 @@ public class CoffeeScriptFlexLexer extends FlexAdapter {
     super(new CoffeeScriptLexer((Reader) null));
   }
 
+  /**
+   * This start implementation first advances the lexer from the start of the file to the start offset, so
+   * that the complex state stack is reconstructed.
+   *
+   * @param buffer The content to lex
+   * @param startOffset The start offset
+   * @param endOffset The end offset
+   * @param initialState The state to start with
+   */
   public void start(final CharSequence buffer, int startOffset, int endOffset, final int initialState) {
-    super.start(buffer, 0, endOffset, initialState);
+    if (startOffset != 0) {
+      super.start(buffer, 0, startOffset, initialState);
+
+      while (getTokenType() != null) {
+        advance();
+      }
+    }
+
+    super.start(buffer, startOffset, endOffset, getState());
   }
 
 }
